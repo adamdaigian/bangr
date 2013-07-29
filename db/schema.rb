1b3b31037459be863838b7bdc0d37e0dcc753aa8
adamdaigian <adam.daigian@gmail.com>
2013-07-29 00:49:44 -0700

    annontated models. added controller, model and migration for messages

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130729061246) do
+ActiveRecord::Schema.define(:version => 20130729073610) do
+
+  create_table "messages", :force => true do |t|
+    t.integer  "sender"
+    t.integer  "receiver"
+    t.text     "content"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
